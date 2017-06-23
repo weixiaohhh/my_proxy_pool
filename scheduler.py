21ed12de582734f944e7365d4a983f634f92c622
weixiaohhh <3060106630@qq.com>
2017-06-23 15:28:05 +0800

    Update scheduler.py
    update

--- scheduler.py
+++ scheduler.py
@@ -3,7 +3,6 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from refresh import main, refresh
 
 if __name__ == '__main__':
-    main()
     sched = BlockingScheduler()
     sched.add_job(refresh, 'interval', minutes=5)
     sched.start()
